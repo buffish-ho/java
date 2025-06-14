19ff9e3f83b4947eb8c6e288d71127e69559f4e4
Buffish Ho <buffish.ho@gmail.com>
2025-06-14 20:29:14 +0800

    feat: 新建数据结构目录

--- data-structure/src/com/buffish/data/array/DynamicArray.java
+++ data-structure/src/com/buffish/data/array/DynamicArray.java
@@ -4,4 +4,20 @@ public class DynamicArray {
     private int size = 0; // 数组大小
     private int capacity = 8; // 数组容量
     private int[] array = new int[capacity];
+
+    public void addLast(int elem) {
+//        array[size++] = elem;
+        this.add(size, elem);
+    }
+
+    public void add(int index, int elem) {
+        if (index < 0 || index > size) {
+            throw new Error("传入的数组索引越界");
+        }
+        if (index < size) {
+            System.arraycopy(array, index, array, index + 1, size - index);
+        }
+        array[index] = elem;
+        size++;
+    }
 }
